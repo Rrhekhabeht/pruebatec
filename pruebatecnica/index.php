9306cb6ce814f02c7a6ed388875c8c9e043debcc
Rrhekhabeht <82349756+Rrhekhabeht@users.noreply.github.com>
2021-10-13 12:09:01 -0500

    Add files via upload

--- pruebatecnica/index.php
+++ pruebatecnica/index.php
@@ -1,21 +1,6 @@
 <?php
  include "funciones/Conexion.php";
-
- $url = 'http://country.io/names.json';
-
-	$curlSession = curl_init();
-	curl_setopt($curlSession, CURLOPT_URL, $url);
-	curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
-	curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
-
-	$jsonData = json_decode(curl_exec($curlSession));
-	curl_close($curlSession);
-	
-	$buffer ='<select name="pais" id="Pais">';
-	foreach($jsonData as $key => $name){
-		$buffer .='<option value="'.$key.'">'.$key.' - '.$name.'</option>';
-	}
-	$buffer .= '</select>';	
+ include "funciones/API.php"	
 ?>
 <!DOCTYPE html>
 <html lang="es">
